f3b201210e0e91765588e76bbd33b7c9e0c9d833
kumaryu-w <cloudshell-user@ip-10-140-117-157.ec2.internal>
2025-04-30 01:20:43 +0000

    aaa

--- lambda/index.py
+++ lambda/index.py
@@ -4,6 +4,7 @@ import os
 import boto3
 import re  # 正規表現モジュールをインポート
 from botocore.exceptions import ClientError
+from urllib import request, error
 
 
 # Lambda コンテキストからリージョンを抽出する関数
@@ -18,11 +19,14 @@ def extract_region_from_arn(arn):
 bedrock_client = None
 
 # モデルID
-MODEL_ID = os.environ.get("MODEL_ID", "us.amazon.nova-lite-v1:0")
+# MODEL_ID = os.environ.get("MODEL_ID", "us.amazon.nova-lite-v1:0")
 
 def lambda_handler(event, context):
+    print("run")
     try:
+    
         # コンテキストから実行リージョンを取得し、クライアントを初期化
+        """
         global bedrock_client
         if bedrock_client is None:
             region = extract_region_from_arn(context.invoked_function_arn)
@@ -36,15 +40,15 @@ def lambda_handler(event, context):
         if 'requestContext' in event and 'authorizer' in event['requestContext']:
             user_info = event['requestContext']['authorizer']['claims']
             print(f"Authenticated user: {user_info.get('email') or user_info.get('cognito:username')}")
-        
+        """
         # リクエストボディの解析
         body = json.loads(event['body'])
         message = body['message']
         conversation_history = body.get('conversationHistory', [])
         
         print("Processing message:", message)
-        print("Using model:", MODEL_ID)
-        
+        # print("Using model:", MODEL_ID)
+
         # 会話履歴を使用
         messages = conversation_history.copy()
         
@@ -54,6 +58,7 @@ def lambda_handler(event, context):
             "content": message
         })
         
+        """
         # Nova Liteモデル用のリクエストペイロードを構築
         # 会話履歴を含める
         bedrock_messages = []
@@ -79,27 +84,43 @@ def lambda_handler(event, context):
                 "topP": 0.9
             }
         }
+        """
+
+        request_payload = {"prompt": message,"max_new_tokens": 512, "do_sample": True, "temperature": 0.7, "top_p": 0.9}
+
         
         print("Calling Bedrock invoke_model API with payload:", json.dumps(request_payload))
         
         # invoke_model APIを呼び出し
+        """
         response = bedrock_client.invoke_model(
             modelId=MODEL_ID,
             body=json.dumps(request_payload),
             contentType="application/json"
         )
+        """
+        FASTAPI_URL="https://95b6-35-223-8-118.ngrok-free.app/generate"
+        req = request.Request(FASTAPI_URL, data=json.dumps(request_payload).encode("utf-8"), headers={"Content-Type":"application/json"}, method="POST")
         
+        with request.urlopen(req) as resp:
+            body_str = resp.read().decode("utf-8")
+            response_body = json.loads(body_str)      
+
+        print(response_body)
+
         # レスポンスを解析
-        response_body = json.loads(response['body'].read())
-        print("Bedrock response:", json.dumps(response_body, default=str))
+        # response_body = json.loads(response['body'].read())
+        # print("response:", json.dumps(response_body, default=str))
         
         # 応答の検証
-        if not response_body.get('output') or not response_body['output'].get('message') or not response_body['output']['message'].get('content'):
-            raise Exception("No response content from the model")
-        
+        #if not response_body.get('output') or not response_body['output'].get('message') or not response_body['output']['message'].get('content'):
+        #    raise Exception("No response content from the model api")
+        if "detail" in response_body.keys():
+            raise Exception("No response content from the model api",response_body["detail"])
+
         # アシスタントの応答を取得
-        assistant_response = response_body['output']['message']['content'][0]['text']
-        
+        # assistant_response = response_body['output']['message']['content'][0]['text']
+        assistant_response = response_body["generated_text"]
         # アシスタントの応答を会話履歴に追加
         messages.append({
             "role": "assistant",
@@ -121,7 +142,7 @@ def lambda_handler(event, context):
                 "conversationHistory": messages
             })
         }
-        
+       
     except Exception as error:
         print("Error:", str(error))
         
